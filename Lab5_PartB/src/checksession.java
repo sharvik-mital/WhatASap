fc148aa3217459485aeb3e9b199042b6d4eeea39
Sharvik <sharvik250298@gmail.com>
2018-08-20 22:54:23 +0530

    Update for session checking

--- Lab5_PartB/src/checksession.java
+++ Lab5_PartB/src/checksession.java
@@ -1,3 +1,4 @@
+import java.sql.*;
 import java.io.IOException;
 
 import javax.servlet.*;
@@ -20,5 +21,36 @@ public class checksession {
 		// TODO Auto-generated method stub
 		
 	}
+public int check(HttpServletRequest request,HttpServletResponse response, int thread) throws ServletException,IOException, SQLException {
+		
+		HttpSession session= request.getSession(false);
+		String ID=(String) session.getAttribute("id");
+		try(Connection conn = DriverManager.getConnection(
+	    		"jdbc:postgresql://localhost:5590/WhatASap", "sharvik", "");)
+		{
+		try(PreparedStatement p=conn.prepareStatement("select uid1, uid2 from conversations "
+				+ "where thread_id=? ");){
+			p.setInt(1,thread);
+			ResultSet rst=p.executeQuery();
+			if(rst.next()) 
+			{
+				String uid1=rst.getString(1);
+				String uid2=rst.getString(2);
+				if(!(ID.equals(uid2)||ID.equals(uid1)))
+				{	response.sendRedirect("Home");
+					return 0; 
+				}
+				return 1;
+			}	
+			else
+			{
+				response.sendRedirect("Home");
+				return 0;
+
+			}
+		}
+		}
+		// TODO Auto-generated method stub
+		
+	}
 }
-	
